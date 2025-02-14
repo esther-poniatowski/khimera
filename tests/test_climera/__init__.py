5c2b47cd4bee1235718be31a57da2c11eaa312b0
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2025-02-14 12:10:46 +0100

    feat(climera): Initialize `src` and  `tests`

---
+++ tests/test_climera/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+test_climera
+===============
+
+Tests for the climera package.
+
+Modules
+-------
+
+
+See Also
+--------
+climera
+
+Notes
+-----
+This test package is imported by Sphinx to extract docstrings for documenting tests. This
+documentation provides additional examples and explanations for the tested modules and functions.
+"""
