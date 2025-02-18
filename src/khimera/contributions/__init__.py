ad7170ce9f52f88696628d33aadadd2f5fd6775c
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2025-02-18 16:29:38 +0100

    docs(contributions): Update docstrings

--- src/khimera/contributions/__init__.py
+++ src/khimera/contributions/__init__.py
@@ -6,6 +6,15 @@ khimera.contributions
 
 Components of plugins that contribute to the host application.
 
+Each module in this package encapsulates the behavior of one type of plugin components. Each
+inherits from the base classes `Contrib` and `CategorySpec` and tailors their attributes and methods
+to the specific plugin components they represent.
+
+Both the `Contrib` and `CategorySpec` classes are subclassed in parallel in each module, forming a
+hierarchy of plugin components. The `Contrib` class represents the actual components provided by
+plugin instances, while the `CategorySpec` class represents constraints for those components
+according to the host application's requirements.
+
 Modules
 -------
 core
@@ -18,10 +27,12 @@ api
     API components of plugins.
 hooks
     Hook components of plugins.
-
+assets
+    Asset components of plugins.
 
 
 See Also
 --------
-
+khimera.contributions.core
+    Base components of plugins models and instances.
 """
