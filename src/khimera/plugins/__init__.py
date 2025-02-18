6d75fcca4e79b56ea78034c1c3bc833745ea6317
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2025-02-18 10:29:17 +0100

    feat(plugins): Create module for plugin management

---
+++ src/khimera/plugins/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+khimera.plugins
+===============
+
+Manages discovery, loading and caching of plugins.
+
+Modules
+-------
+discover
+
+
+See Also
+--------
+
+"""
