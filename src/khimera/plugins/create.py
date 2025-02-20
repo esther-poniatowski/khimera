56d2506c36c8e773e4a051ff8a9e99b46203a261
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2025-02-20 08:53:25 +0100

    feat(create): Add mixins in the Plugin

--- src/khimera/plugins/create.py
+++ src/khimera/plugins/create.py
@@ -4,14 +4,12 @@
 khimera.plugins.create
 ======================
 
-Standardized interface for creating plugins, on the plugin provider side.
+Standardized interface for creating plugins (on the plugin provider side).
 
 Classes
 -------
 Plugin
-    Represents the components of a plugin.
-Components
-    Type for a list of components of a plugin.
+    Represents the components provided by a plugin to the host application.
 
 See Also
 --------
@@ -19,16 +17,16 @@ khimera.plugins.core
 khimera.plugins.declare
 """
 from typing import Optional, Type, Dict, Self
-import copy
 
-from khimera.utils.factories import TypeConstrainedDict, TypeConstrainedList
+from khimera.utils.factories import TypeConstrainedDict
+from khimera.utils.mixins import DeepCopyable, DeepComparable
 from khimera.components.core import Component, ComponentSet
 from khimera.plugins.declare import PluginModel
 
 
-class Plugin:
+class Plugin(DeepCopyable, DeepComparable):
     """
-    Represents the components of a plugin.
+    Represents the components provided by a plugin to the host application.
 
     Attributes
     ----------
@@ -37,10 +35,26 @@ class Plugin:
     name : str
         Name of the plugin.
     components : Dict[str, ComponentSet]
-        Components of the plugin for each specification field in the plugin model.
-        Keys: Names of the specification fields declared in the plugin model.
-        Values: Contribution(s) for each field. If the specification sets expects one unique
-        component, the list should contain only one element.
+        Components of the plugin for each field specified in the plugin model.
+        Keys: Fields declared in the plugin model.
+        Values: Container of component(s) for each field. If the field expects one unique component,
+        the container should include only one element.
+
+    Methods
+    -------
+    add(key: str, comp: Component) -> Self
+        Add a component to one of the specified fields in the plugin model.
+    remove(key: str, comp_name: Optional[str] = None) -> Self
+        Remove a component or all components for a specific key from the plugin.
+    get(key: str, names: bool = False) -> ComponentSet
+        Get the components of the plugin for a specific field.
+    filter(category: Optional[Type[Component]] = None) -> Dict[str, ComponentSet]
+        Get the components of the plugin, optionally filtered by category.
+    copy() -> Self
+        Create a deep copy of the plugin (provided by the `DeepCopyable` mixin).
+    __eq__(other: Self) -> bool
+        Compare the plugin with another plugin by deep comparison (provided by the `DeepComparable`
+        mixin).
 
     Examples
     --------
@@ -72,18 +86,18 @@ class Plugin:
 
     Notes
     -----
-    No validation of the components is performed when adding components to the plugin. The
-    components are validated against the plugin model by the `PluginValidator` class in the
-    module `khimera.plugins.validate`.
+    No validation of the components is performed when adding them to the plugin. The components are
+    validated against the plugin model by the `PluginValidator` class in the module
+    `khimera.plugins.validate`.
 
     Implications:
 
-    - Any field (key) can be added to the plugin, regardless of the presence of the corresponding
+    - New fields (keys) can be added to the plugin, regardless of the presence of the corresponding
       field in the model.
-    - Multiple components can be added to the same field, regardless of the uniqueness
-      constraints set by the corresponding field in the model.
+    - Multiple components can be added to the same field, regardless of the uniqueness constraints
+      set by the corresponding `FieldSpec` in the model.
     - Any type of component can be provided to the plugin, regardless of the expected category of
-      the corresponding field in the model.
+      the corresponding `FieldSpec` in the model.
     """
     def __init__(self,
                 model : PluginModel,
@@ -96,21 +110,24 @@ class Plugin:
         self.version = version
         self.components = TypeConstrainedDict(str, ComponentSet) # automatic type checking
         for key, value in kwargs.items(): # add immediate contents of kwargs
-            self.add(key=key, contrib=value)
+            self.add(key=key, comp=value)
 
     def __str__(self):
-        """Print name and metadata of the plugin."""
-        return f"Plugin(name={self.name}, version={self.version}, model={self.model})"
+        fields_str = ", ".join(f"{key}" for key in self.components)
+        return f"{self.__class__.__name__}(name='{self.name}', version='{self.version}', model={self.model}):[{fields_str}]"
 
-    def add(self, key: str, contrib: Component) -> Self:
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name='{self.name}', version='{self.version}', model={self.model})"
+
+    def add(self, key: str, comp: Component) -> Self:
         """
         Add a component to one of the specified fields in the plugin model.
 
         Arguments
         ---------
         key : str
-            Key of the specification field in the plugin model.
-        contrib : Component
+            Key of the field as defined in the plugin model.
+        comp : Component
             Contribution to add to the plugin.
 
         Returns
@@ -121,24 +138,26 @@ class Plugin:
         Raise
         -----
         TypeError
-            If the `contrib` argument is not a subclass of `Component`. Automatically raised by the
+            If the `comp` argument is not a subclass of `Component`. Automatically raised by the
             `TypeConstrainedList` class when adding the component to the list of components.
         """
         if key not in self.components: # initialize storage for the field
             self.components[key] = ComponentSet() # automatic type checking
-        contrib.attach(self.name) # keep track of the plugin providing the component
-        self.components[key].append(contrib)
+        if comp.name in self.get(key, names=True): # check for duplicate component names
+            raise AttributeError(f"Duplicate component '{comp.name}' for field '{key}'")
+        self.components[key].append(comp)
+        comp.attach(self.name) # keep track of the plugin providing the component
         return self
 
-    def remove(self, key: str, contrib_name: Optional[str] = None) -> Self:
+    def remove(self, key: str, comp_name: Optional[str] = None) -> Self:
         """
         Remove a component or all components for a specific key from the plugin.
 
         Arguments
         ---------
         key : str
-            Key of the specification field in the plugin model.
-        contrib_name : str, optional
+            Key of the field as defined in the plugin model.
+        comp_name : str, optional
             Name of the specific component to remove. If not provided, all components for the
             key are removed.
 
@@ -156,31 +175,36 @@ class Plugin:
         """
         if key not in self.components:
             raise KeyError(f"No key '{key}' in the plugin's components")
-        if contrib_name is None:
+        if comp_name is None:
             del self.components[key]
         else:
             try:
-                contrib = next(contrib for contrib in self.components[key] if contrib.name == contrib_name)
-                self.components[key].remove(contrib)
-            except ValueError:
-                raise ValueError(f"No component '{contrib_name}' for key '{key}'")
+                comp = next(comp for comp in self.components[key] if comp.name == comp_name)
+                self.components[key].remove(comp)
+            except (ValueError, KeyError, StopIteration):
+                raise KeyError(f"No component '{comp_name}' for key '{key}'")
         return self
 
-    def get(self, key: str) -> ComponentSet:
+    def get(self, key: str, names : bool = False) -> ComponentSet:
         """
         Get the components of the plugin for a specific field.
 
         Arguments
         ---------
         key : str
-            Key of the field in the plugin model.
+            Key of the field in the plugin instance.
+        names : bool, optional
+            If True, return the names of the components instead of the components themselves.
 
         Returns
         -------
         ComponentSet
             Components of the plugin stored for the specified field.
         """
-        return self.components.get(key, [])
+        components = self.components.get(key, [])
+        if names:
+            return [comp.name for comp in components]
+        return components
 
     def filter(self, category: Optional[Type[Component]] = None) -> Dict[str, ComponentSet]:
         """
@@ -189,8 +213,7 @@ class Plugin:
         Arguments
         ---------
         category : Type[Component]
-            Category of the components to filter. If not provided, all components are
-            returned.
+            Category of the components to filter. If not provided, all components are returned.
 
         Returns
         -------
@@ -200,18 +223,3 @@ class Plugin:
         if category:
             return {key: contribs for key, contribs in self.components.items() if any(isinstance(item, category) for item in contribs)}
         return self.components
-
-    def copy(self) -> Self:
-        """
-        Create a deep copy of the plugin, creating copies of all its nested components.
-
-        Returns
-        -------
-        Plugin
-            Copy of the plugin instance.
-
-        See Also
-        --------
-        copy.deepcopy
-        """
-        return copy.deepcopy(self)
--- tests/test_khimera/test_plugins/test_create.py
+++ tests/test_khimera/test_plugins/test_create.py
@@ -11,7 +11,6 @@ See Also
 khimera.plugins.create
 """
 import pytest
-from typing import Type
 
 from khimera.plugins.create import Plugin
 from khimera.components.core import FieldSpec, Component
@@ -25,9 +24,10 @@ class MockSpec(FieldSpec):
     def __init__(self, name: str):
         self.name = name
 
-    def validate(self, contrib) -> bool:
+    def validate(self, comp) -> bool:
         """Implement abstract method for validation."""
         return True
+
 
 @pytest.fixture
 def mock_model():
@@ -36,8 +36,15 @@ def mock_model():
     model.add(MockSpec(name="test_spec"))
     return model
 
-class MockContrib(Component):
+
+class MockComponent(Component):
     """Mock component class for testing."""
+    def __init__(self, name: str):
+        self.name = name
+
+
+class MockComponent2(Component):
+    """Mock component class for testing different categories."""
     def __init__(self, name: str):
         self.name = name
 
@@ -54,6 +61,28 @@ def test_plugin_initialization(mock_model):
     assert plugin.model == mock_model
     assert len(plugin.components) == 0
 
+
+@pytest.mark.parametrize("names, expected", [
+    (False, lambda comp: comp),
+    (True, lambda comp: comp.name)
+])
+def test_get_component(mock_model, names, expected):
+    """
+    Test retrieving a component from a plugin.
+
+    Notes
+    -----
+    Test `get` before `add` since the `get` method is used in `add`.
+    """
+    plugin = Plugin(model=mock_model, name="test_plugin")
+    field_key = "test_spec"
+    comp = MockComponent(name="test_comp")
+    plugin.add(key=field_key, comp=comp)
+    retrieved = plugin.get(field_key, names=names)
+    assert len(retrieved) == 1
+    assert retrieved[0] == expected(comp)
+
+
 @pytest.mark.parametrize("field_name, expected_field_exists", [
     ("test_spec", True),
     ("new_spec", False)
@@ -61,44 +90,84 @@ def test_plugin_initialization(mock_model):
 def test_add_component(mock_model, field_name, expected_field_exists):
     """Test adding a component to a plugin in various fields."""
     plugin = Plugin(name="test_plugin", model=mock_model)
-    contrib = MockContrib(name="test_contrib")
-    plugin.add(field_name, contrib)
+    comp = MockComponent(name="test_contrib")
+    plugin.add(field_name, comp)
     assert field_name in plugin.components
     assert len(plugin.components[field_name]) == 1
-    assert plugin.components[field_name][0] == contrib
+    assert plugin.components[field_name][0] == comp
     assert (field_name in mock_model.fields) == expected_field_exists
 
-def test_add_duplicate_component():
-    model = MockSpec(name="test_model")
-    plugin = Plugin(model=model)
 
-    command_spec = CommandSpec(name="test_command")
-    plugin.add("commands", command_spec)
-
+def test_add_duplicate_component(mock_model):
+    plugin = Plugin(model=mock_model, name="test_plugin")
+    field_key = "test_spec"
+    comp = MockComponent(name="test_comp")
+    plugin.add(key=field_key, comp=comp)
     with pytest.raises(AttributeError):
-        plugin.add("commands", command_spec)
+        plugin.add(key=field_key, comp=comp)
 
-def test_get_component():
+
+def test_remove_component(mock_model):
+    """Test removing a named component from a plugin."""
+    plugin = Plugin(model=mock_model, name="test_plugin")
+    field_key = "test_spec"
+    comp = MockComponent(name="test_comp")
+    plugin.add(key=field_key, comp=comp)
+    plugin.remove(key=field_key, comp_name=comp.name)
+    assert len(plugin.components[field_key]) == 0
+
+
+def test_remove_field(mock_model):
+    """Test removing a field (no `comp_name` argument)."""
+    plugin = Plugin(model=mock_model, name="test_plugin")
+    field_key = "test_spec"
+    comp = MockComponent(name="test_comp")
+    plugin.add(key=field_key, comp=comp)
+    plugin.remove(key=field_key) # no `comp_name` argument
+    assert field_key not in plugin.components
+
+
+def test_remove_nonexistent_component(mock_model):
+    plugin = Plugin(model=mock_model, name="test_plugin")
+    field_key = "test_spec"
+    comp = MockComponent(name="test_comp")
+    plugin.add(key=field_key, comp=comp) # initialize the field
+    with pytest.raises(KeyError):
+        plugin.remove(key=field_key, comp_name="nonexistent")
+
+
+def test_filter_components(mock_model):
+    """Test filtering components from a plugin by category."""
     model = MockSpec(name="test_model")
-    plugin = Plugin(model=model)
+    plugin = Plugin(model=mock_model, name="test_plugin")
+    spec1 = MockComponent(name="comp2")
+    spec2 = MockComponent2(name="comp1")
+    field_key = "test_spec"
+    plugin.add(key=field_key, comp=spec1)
+    plugin.add(key=field_key, comp=spec2)
+    filtered = plugin.filter(category=MockComponent)
+    assert len(filtered) == 1
 
-    command_spec = CommandSpec(name="test_command")
-    plugin.add("commands", command_spec)
 
-    retrieved = plugin.get("commands")
-    assert len(retrieved) == 1
-    assert retrieved[0] == command_spec
+def test_copy(mock_model):
+    """Test copying a plugin instance (via the `DeepCopyable` mixin)."""
+    plugin = Plugin(model=mock_model, name="test_plugin")
+    comp = MockComponent(name="test_comp")
+    plugin.add(key="test_spec", comp=comp)
+    copied = plugin.copy()
+    assert copied.name == plugin.name
+    assert copied.version == plugin.version
+    assert copied is not plugin
+    assert copied.components == plugin.components
+    assert copied.model == plugin.model
 
-def test_filter_components():
-    model = MockSpec(name="test_model")
-    plugin = Plugin(model=model)
 
-    command_spec1 = CommandSpec(name="command1")
-    command_spec2 = CommandSpec(name="command2")
-
-    plugin.add("commands", command_spec1)
-    plugin.add("commands", command_spec2)
-
-    filtered = plugin.filter(category=CommandSpec)
-
-    assert len(filtered) == 2
+def test_equality(mock_model):
+    """Test equality comparison of plugin instances (via the `DeepComparable` mixin)."""
+    plugin1 = Plugin(model=mock_model, name="test_plugin")
+    plugin2 = Plugin(model=mock_model, name="test_plugin")
+    assert plugin1 == plugin2
+    plugin1.add(key="test_spec", comp=MockComponent(name="test_comp"))
+    assert plugin1 != plugin2
+    plugin2.add(key="test_spec", comp=MockComponent(name="test_comp"))
+    assert plugin1 == plugin2
