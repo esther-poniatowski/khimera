9af73876df143f5b2c8cf7ec4541f02347477b1d
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2025-02-20 08:34:02 +0100

    feat(utils): Ad mixins for deep copy and deep comparison

---
+++ src/khimera/utils/mixins.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+khimera.utils.mixins
+====================
+
+Mixin classes for common functionality in custom classes and objects with nested components.
+
+Classes
+-------
+DeepCopyable
+    Mixin class for creating deep copies of objects.
+DeepComparable
+    Mixin class for comparing objects by deep comparison.
+"""
+import copy
+from typing import Self
+
+from deepdiff import DeepDiff, DeepHash
+
+
+class DeepCopyable:
+    """
+    Mixin class for creating deep copies of objects.
+
+    Methods
+    -------
+    copy() -> Self
+        Create a deep copy of the object.
+
+    See Also
+    --------
+    copy.deepcopy
+    """
+    def copy(self) -> Self:
+        """Create a deep copy of the object, creating copies of all its nested components."""
+        return copy.deepcopy(self)
+
+
+class DeepComparable:
+    """
+    Mixin class for comparing objects by deep comparison.
+
+    Methods
+    -------
+    __eq__(other: Self) -> bool
+        Compare the object with another object by deep comparison.
+
+    See Also
+    --------
+    deepdiff.DeepDiff
+    """
+    def __eq__(self, other):
+        """Compare the object with another object by deep comparison."""
+        if not isinstance(other, self.__class__):
+            return False
+        diff = DeepDiff(self.__dict__, other.__dict__, ignore_order=True)
+        return not diff  # True if no differences
---
+++ tests/test_khimera/test_utils/test_mixins.py
@@ -0,0 +1,73 @@
+import unittest
+from src.khimera.utils.mixins import DeepCopyable, DeepComparable, DeepHashable
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+test_khimera.test_utils.test_mixins
+===================================
+
+Tests for the mixin classes for common functionality in custom classes and objects with nested
+components.
+
+See Also
+--------
+khimera.utils.mixins
+"""
+import pytest
+
+from khimera.utils.mixins import DeepCopyable, DeepComparable
+
+
+# --- Mock Class for Testing Mixins ----------------------------------------------------------------
+
+class NestedObject(DeepCopyable, DeepComparable):
+    """Class for testing mixins with nested objects."""
+    def __init__(self, value):
+        self.value = value
+
+class TestClass(DeepCopyable, DeepComparable):
+    """Class which uses the DeepCopyable, DeepComparable, and DeepHashable mixins."""
+    def __init__(self, nested, *args):
+        self.mutable = [*args]
+        self.nested = nested
+
+
+# --- Tests for DeepCopyable -----------------------------------------------------------------------
+
+def test_deep_copyable():
+    """
+    Test the `DeepCopyable` mixin.
+
+    Expected Behavior:
+
+    - The deep copy should be a new object, with a new mutable object and a new nested object.
+    - The deep copy should have the same values as the original object.
+    """
+    old = TestClass(NestedObject(1), 1, 2, 3)
+    new = old.copy()
+    assert old is not new
+    assert old.mutable is not new.mutable
+    assert old.nested is not new.nested
+    assert old.mutable == new.mutable
+    assert old.nested.value == new.nested.value
+
+
+# --- Tests for DeepComparable ---------------------------------------------------------------------
+
+def test_deep_comparable():
+    """
+    Test the `DeepComparable` mixin.
+
+    Expected Behavior:
+
+    - The equality operators should be available.
+    - It should return True for objects with the same values and False for objects with different
+      values.
+    """
+    obj1 = TestClass(NestedObject(1), 1, 2, 3)
+    obj2 = TestClass(NestedObject(1), 1, 2, 3)
+    obj3 = TestClass(NestedObject(2), 1, 2, 3)
+    assert obj1 == obj2
+    assert not obj1 == obj3
+    assert obj1 != obj3
