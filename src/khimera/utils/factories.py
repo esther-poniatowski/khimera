fabac12bdea21b3864b8b5a3abef5478b97140f4
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2025-03-02 12:39:22 +0100

    fix(factories): Correct typing and linter errors

--- src/khimera/utils/factories.py
+++ src/khimera/utils/factories.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
@@ -8,16 +7,21 @@ khimera.utils.factories
 Utility classes and functions for creating default objects.
 """
 from collections import UserDict, UserList
-from typing import Generic, TypeVar, Type, Union, Optional, Iterable, Any, Dict
+from typing import Generic, TypeVar, Type, Union, Optional, Iterable, Any, Dict, Tuple
 from typing_extensions import get_origin, get_args
 
-KT = TypeVar('KT')
-VT = TypeVar('VT')
+
+KT = TypeVar("KT")
+"""Type variable for the key type of a dictionary."""
+
+VT = TypeVar("VT")
+"""Type variable for the value type of a dictionary or list."""
 
 
 # --- Type Utilities -------------------------------------------------------------------------------
 
-def get_union_types(tp: Type) -> Optional[list]:
+
+def get_union_types(tp: Type) -> Optional[Tuple[Type, ...]]:
     """
     Get the types of a Union type.
 
@@ -28,8 +32,8 @@ def get_union_types(tp: Type) -> Optional[list]:
 
     Returns
     -------
-    Optional[list]
-        List of types if the type is a Union, otherwise None.
+    Optional[Tuple[Type, ...]]
+        All the types if the type is a Union, otherwise None.
 
     See Also
     --------
@@ -47,17 +51,18 @@ def get_union_types(tp: Type) -> Optional[list]:
 
     >>> from typing import Union
     >>> get_union_types(Union[int, str])
-    [int, str]
+    (int, str)
 
     Get the types of a non-Union type:
 
     >>> get_union_types(int)
     None
     """
-    return get_args(tp) if get_origin(tp) is Union else None
+    return tuple(get_args(tp)) if get_origin(tp) is Union else None
 
 
 # --- Type-Constrained Containers ------------------------------------------------------------------
+
 
 class TypeConstrainedDict(UserDict[KT, VT], Generic[KT, VT]):
     """
@@ -99,7 +104,10 @@ class TypeConstrainedDict(UserDict[KT, VT], Generic[KT, VT]):
     --------
     collections.UserDict
     """
-    def __init__(self, key_type: Type[KT], value_type: Type[VT], data: Dict[KT, VT] = None):
+
+    def __init__(
+        self, key_type: Type[KT], value_type: Type[VT], data: Optional[Dict[KT, VT]] = None
+    ):
         """
         Initialize the dictionary with the types of its keys and values.
 
@@ -122,49 +130,73 @@ class TypeConstrainedDict(UserDict[KT, VT], Generic[KT, VT]):
 
     def is_valid_key_type(self, key: Any) -> bool:
         """Check if the key is of the correct type using the Union types if present."""
-        return (any(isinstance(key, t) for t in self._key_union_types)
-                if self._key_union_types else isinstance(key, self.key_type))
+        return (
+            any(isinstance(key, t) for t in self._key_union_types)
+            if self._key_union_types
+            else isinstance(key, self.key_type)
+        )
 
     def is_valid_value_type(self, value: Any) -> bool:
         """Check if the value is of the correct type using the Union types if present."""
-        return (any(isinstance(value, t) for t in self._value_union_types)
-                if self._value_union_types else isinstance(value, self.value_type))
+        return (
+            any(isinstance(value, t) for t in self._value_union_types)
+            if self._value_union_types
+            else isinstance(value, self.value_type)
+        )
 
-    def error_message(self, item: Any, expected_type: Type, k_or_v : Optional[str] = None) -> str:
+    def error_message(self, item: Any, expected_type: Type, k_or_v: Optional[str] = None) -> str:
         """Return an error message for an invalid item."""
         actual_type = type(item).__name__
         if get_origin(expected_type) is Union:
-            expected = f"Union[{', '.join(t.__name__ for t in get_args(expected_type))}]"
+            expected = f"Union[{", ".join(t.__name__ for t in get_args(expected_type))}]"
         else:
             expected = expected_type.__name__
         return f"Invalid {k_or_v} type: got {actual_type} instead of {expected}"
 
     def __setitem__(self, key: KT, value: VT) -> None:
-        """Override the setter method of `dict` to constrain the types of the assigned key and value."""
+        """Override `dict` setter method to constrain the types of the assigned key and value."""
         if not self.is_valid_key_type(key):
             raise TypeError(self.error_message(key, self.key_type, k_or_v="key"))
         if not self.is_valid_value_type(value):
             raise TypeError(self.error_message(value, self.value_type, k_or_v="value"))
         super().__setitem__(key, value)
 
-    def update(self, *args, **kwargs):
-        """Override the update method of `dict` to constrain the types of the updated keys and values."""
-        if args:
-            other = args[0]
-            if isinstance(other, dict):
-                for key, value in other.items():
-                    self[key] = value
-            elif hasattr(other, "keys"):
-                for key in other.keys():
-                    self[key] = other[key]
-            else:
-                for key, value in other:
-                    self[key] = value
-        for key, value in kwargs.items():
-            self[key] = value
+    def update(self, other=(), /, **kwargs):
+        """Override `dict` update method to constrain the types of the updated keys and values.
+
+        Arguments
+        ---------
+        other : Union[Dict[KT, VT], Iterable[Tuple[KT, VT]]], optional
+            Dictionary or iterable of key-value pairs to update the dictionary with.
+        **kwargs : Dict[KT, VT]
+            Key-value pairs to update the dictionary with.
+
+        See Also
+        --------
+        collections.MutableMapping.update
+            Original method documentation, using `overload` to provide various type signatures.
+            See mypy issue  #1430.
+        """
+        keys, values = (), ()  # initialize containers for keys and values to check
+        if isinstance(other, dict):  # `other` is dict: check types for each (key, value) pair
+            keys += tuple(other.keys())
+            values += tuple(other.values())
+        elif isinstance(other, Iterable):  # `other` is iterable: check types for each pair
+            keys += tuple(k for k, _ in other)
+            values += tuple(v for _, v in other)
+        if kwargs:  # `kwargs` is dict: add keys and values to the existing ones
+            keys += tuple(kwargs.keys())
+            values += tuple(kwargs.values())
+        for key, value in zip(keys, values):
+            if not self.is_valid_key_type(key):
+                raise TypeError(self.error_message(key, self.key_type, k_or_v="key"))
+            if not self.is_valid_value_type(value):
+                raise TypeError(self.error_message(value, self.value_type, k_or_v="value"))
+        super().update(other, **kwargs)
 
 
 # --- Type-Constrained List ------------------------------------------------------------------------
+
 
 class TypeConstrainedList(UserList[VT], Generic[VT]):
     """
@@ -202,6 +234,7 @@ class TypeConstrainedList(UserList[VT], Generic[VT]):
     --------
     collections.UserList
     """
+
     def __init__(self, value_type: Type[VT], data: Optional[Iterable[VT]] = None):
         """
         Initialize the list with the type of its elements.
@@ -220,14 +253,17 @@ class TypeConstrainedList(UserList[VT], Generic[VT]):
             self.extend(data)
 
     def is_valid_type(self, value: Any) -> bool:
-        """Check if the value is of the correct type using the Union types if present."""
-        return (any(isinstance(value, t) for t in self._union_types)
-                if self._union_types else isinstance(value, self.value_type))
+        """Check if the value is of the correct type, using the Union types if present."""
+        if self._union_types:
+            return any(isinstance(value, t) for t in self._union_types)
+        return isinstance(value, self.value_type)
 
     def error_message(self, value: Any) -> str:
         """Return an error message for an invalid value."""
-        expected_type = (f"Union[{', '.join(t.__name__ for t in self._union_types)}]"
-                        if self._union_types else self.value_type.__name__)
+        if self._union_types:
+            expected_type = f"Union[{", ".join(t.__name__ for t in self._union_types)}]"
+        else:
+            expected_type = self.value_type.__name__
         return f"Invalid value type: got {type(value).__name__} instead of {expected_type}"
 
     def __setitem__(self, index, value):
@@ -239,15 +275,15 @@ class TypeConstrainedList(UserList[VT], Generic[VT]):
             raise TypeError(self.error_message(value))
         super().__setitem__(index, value)
 
-    def append(self, value: VT) -> None:
+    def append(self, item: VT) -> None:
         """Override the append method of `list` to constrain the type of the appended value."""
-        if not self.is_valid_type(value):
-            raise TypeError(self.error_message(value))
-        super().append(value)
+        if not self.is_valid_type(item):
+            raise TypeError(self.error_message(item))
+        super().append(item)
 
-    def extend(self, values: Iterable[VT]) -> None:
+    def extend(self, other: Iterable[VT]) -> None:
         """Override the extend method of `list` to constrain the types of the extended values."""
-        for value in values:
+        for value in other:
             if not self.is_valid_type(value):
                 raise TypeError(self.error_message(value))
-        super().extend(values)
+        super().extend(other)
--- tests/test_khimera/test_utils/test_factories.py
+++ tests/test_khimera/test_utils/test_factories.py
@@ -10,33 +10,49 @@ See Also
 --------
 khimera.utils.factories
 """
+# --- Silenced Errors ---
+# pylint: disable=unused-variable
+#   Test functions are used, but pylint does not detect it.
+
+
 from collections import UserDict
-import pytest
 import re
 from typing import Union, List, Dict, Any
+
+import pytest
 
 from khimera.utils.factories import TypeConstrainedList, TypeConstrainedDict, get_union_types
 
 
 # --- Type Utilities -------------------------------------------------------------------------------
 
+
 def test_get_union_types_with_simple_union():
     assert get_union_types(Union[int, str]) == (int, str)
 
+
 def test_get_union_types_with_complex_union():
-    assert get_union_types(Union[int, str, List[float], Dict[str, Any]]) == (int, str, List[float], Dict[str, Any])
+    assert get_union_types(Union[List[float], Dict[str, Any]]) == (
+        List[float],
+        Dict[str, Any],
+    )
+
 
 def test_get_union_types_with_nested_union():
     assert get_union_types(Union[int, Union[str, float]]) == (int, str, float)
 
+
 def test_get_union_types_with_non_union():
     assert get_union_types(int) is None
+
 
 def test_get_union_types_with_list():
     assert get_union_types(List[int]) is None
 
+
 def test_get_union_types_with_dict():
     assert get_union_types(Dict[str, int]) is None
+
 
 def test_get_union_types_with_any():
     assert get_union_types(Any) is None
@@ -44,31 +60,37 @@ def test_get_union_types_with_any():
 
 # --- Tests for TypeConstrainedList ----------------------------------------------------------------
 
+
 def test_type_constrained_list_is_valid_type():
     """Test the `is_valid_type` method of TypeConstrainedList."""
     l = TypeConstrainedList(int)
-    assert l.is_valid_type(1) == True
-    assert l.is_valid_type('2') == False
+    assert l.is_valid_type(1)
+    assert not l.is_valid_type("2")
+
 
 def test_type_constrained_list_error_message():
     """Test the error_message method of TypeConstrainedList."""
     l = TypeConstrainedList(int)
-    assert l.error_message('2') == "Invalid value type: got str instead of int"
+    assert l.error_message("2") == "Invalid value type: got str instead of int"
+
 
 def test_type_constrained_list_union_type_error_message():
     """Test the error_message method of TypeConstrainedList with Union type."""
     l = TypeConstrainedList(Union[int, str])
     assert l.error_message(3.0) == "Invalid value type: got float instead of Union[int, str]"
 
+
 def test_type_constrained_list_init_with_valid_elements():
     """Test initializing TypeConstrainedList with valid elements."""
     l = TypeConstrainedList(int, [1, 2])
     assert l == [1, 2]
 
+
 def test_type_constrained_list_init_with_invalid_elements():
     """Test initializing TypeConstrainedList with invalid elements."""
     with pytest.raises(TypeError, match="Invalid value type: got str instead of int"):
-        TypeConstrainedList(int, [1, '2'])
+        TypeConstrainedList(int, [1, "2"])
+
 
 def test_type_constrained_list_append_valid():
     """Test appending valid elements to TypeConstrainedList."""
@@ -77,20 +99,25 @@ def test_type_constrained_list_append_valid():
     l.append(2)
     assert l == [1, 2]
 
+
 def test_type_constrained_list_append_invalid():
     """Test appending invalid elements to TypeConstrainedList."""
     l = TypeConstrainedList(int)
     with pytest.raises(TypeError, match="Invalid value type: got str instead of int"):
-        l.append('2')
+        l.append("2")
+
 
 def test_type_constrained_list_union_type():
     """Test TypeConstrainedList with Union type."""
     l = TypeConstrainedList(Union[int, str])
     l.append(1)
-    l.append('2')
-    assert l == [1, '2']
-    with pytest.raises(TypeError, match=re.escape("Invalid value type: got float instead of Union[int, str]")):
+    l.append("2")
+    assert l == [1, "2"]
+    with pytest.raises(
+        TypeError, match=re.escape("Invalid value type: got float instead of Union[int, str]")
+    ):
         l.append(3.0)
+
 
 def test_type_constrained_list_extend_valid():
     """Test extending TypeConstrainedList with valid elements."""
@@ -98,11 +125,13 @@ def test_type_constrained_list_extend_valid():
     l.extend([2, 3])
     assert l == [1, 2, 3]
 
+
 def test_type_constrained_list_extend_invalid():
     """Test extending TypeConstrainedList with invalid elements."""
     l = TypeConstrainedList(int, [1])
     with pytest.raises(TypeError, match="Invalid value type: got str instead of int"):
-        l.extend([2, '3'])
+        l.extend([2, "3"])
+
 
 def test_type_constrained_list_setitem_valid():
     """Test setting valid items in TypeConstrainedList."""
@@ -110,11 +139,13 @@ def test_type_constrained_list_setitem_valid():
     l[1] = 4
     assert l == [1, 4, 3]
 
+
 def test_type_constrained_list_setitem_invalid():
     """Test setting invalid items in TypeConstrainedList."""
     l = TypeConstrainedList(int, [1, 2, 3])
     with pytest.raises(TypeError, match="Invalid value type: got str instead of int"):
-        l[1] = '4'
+        l[1] = "4"
+
 
 def test_type_constrained_list_slice_assignment_valid():
     """Test slice assignment with valid items in TypeConstrainedList."""
@@ -122,34 +153,39 @@ def test_type_constrained_list_slice_assignment_valid():
     l[1:3] = [5, 6]
     assert l == [1, 5, 6, 4]
 
+
 def test_type_constrained_list_slice_assignment_invalid():
     """Test slice assignment with invalid items in TypeConstrainedList."""
     l = TypeConstrainedList(int, [1, 2, 3, 4])
     with pytest.raises(TypeError, match="Invalid values, not all of type"):
-        l[1:3] = [5, '6']
+        l[1:3] = [5, "6"]
 
 
 # --- Tests for TypeConstrainedDict ----------------------------------------------------------------
 
+
 def test_type_constrained_dict_is_valid_key_type():
     """Test `is_valid_key_type` method."""
     d = TypeConstrainedDict(Union[str, int], Any)
-    assert d.is_valid_key_type('a') == True
-    assert d.is_valid_key_type(1) == True
-    assert d.is_valid_key_type(1.0) == False
+    assert d.is_valid_key_type("a")
+    assert d.is_valid_key_type(1)
+    assert not d.is_valid_key_type(1.0)
+
 
 def test_type_constrained_dict_is_valid_value_type():
     """Test `is_valid_value_type` method."""
     d = TypeConstrainedDict(str, Union[int, str])
-    assert d.is_valid_value_type(1) == True
-    assert d.is_valid_value_type('a') == True
-    assert d.is_valid_value_type(1.0) == False
+    assert d.is_valid_value_type(1)
+    assert d.is_valid_value_type("a")
+    assert not d.is_valid_value_type(1.0)
+
 
 def test_type_constrained_dict_error_message():
     """Test error_message method."""
     d = TypeConstrainedDict(str, int)
-    assert d.error_message(1.0, int, 'value') == "Invalid value type: got float instead of int"
-    assert d.error_message('a', int, 'key') == "Invalid key type: got str instead of int"
+    assert d.error_message(1.0, int, "value") == "Invalid value type: got float instead of int"
+    assert d.error_message("a", int, "key") == "Invalid key type: got str instead of int"
+
 
 def test_type_constrained_dict_init():
     """Test initialization of TypeConstrainedDict."""
@@ -157,23 +193,27 @@ def test_type_constrained_dict_init():
     assert isinstance(d, UserDict)
     assert d.key_type == str
     assert d.value_type == int
-    assert len(d) == 0
+    assert not d  # len(d) == 0
+
 
 def test_type_constrained_dict_init_with_valid_data():
     """Test initialization of TypeConstrainedDict with initial data."""
-    d = TypeConstrainedDict(str, int, {'a': 1, 'b': 2})
-    assert d == {'a': 1, 'b': 2}
+    d = TypeConstrainedDict(str, int, {"a": 1, "b": 2})
+    assert d == {"a": 1, "b": 2}
+
 
 def test_type_constrained_dict_init_with_invalid_data():
     """Test initialization of TypeConstrainedDict with invalid initial data."""
     with pytest.raises(TypeError, match="Invalid value type: got str instead of int"):
-        TypeConstrainedDict(str, int, {'a': 1, 'b': '2'})
+        TypeConstrainedDict(str, int, {"a": 1, "b": "2"})
+
 
 def test_type_constrained_dict_setitem_valid():
     """Test setting valid items in TypeConstrainedDict."""
     d = TypeConstrainedDict(str, int)
-    d['a'] = 1
-    assert d == {'a': 1}
+    d["a"] = 1
+    assert d == {"a": 1}
+
 
 def test_type_constrained_dict_setitem_invalid_key():
     """Test setting items with invalid key type in TypeConstrainedDict."""
@@ -181,53 +221,66 @@ def test_type_constrained_dict_setitem_invalid_key():
     with pytest.raises(TypeError, match="Invalid key type: got int instead of str"):
         d[1] = 1
 
+
 def test_type_constrained_dict_setitem_invalid_value():
     """Test setting items with invalid value type in TypeConstrainedDict."""
     d = TypeConstrainedDict(str, int)
     with pytest.raises(TypeError, match="Invalid value type: got str instead of int"):
-        d['a'] = '1'
+        d["a"] = "1"
+
 
 def test_type_constrained_dict_union_type():
     """Test TypeConstrainedDict with Union types."""
     d = TypeConstrainedDict(str, Union[int, str])
-    d['a'] = 1
-    d['b'] = '2'
-    assert d == {'a': 1, 'b': '2'}
-    with pytest.raises(TypeError, match=re.escape("Invalid value type: got float instead of Union[int, str]")):
-        d['c'] = 3.0
+    d["a"] = 1
+    d["b"] = "2"
+    assert d == {"a": 1, "b": "2"}
+    with pytest.raises(
+        TypeError, match=re.escape("Invalid value type: got float instead of Union[int, str]")
+    ):
+        d["c"] = 3.0
+
 
 def test_type_constrained_dict_update_valid():
     """Test updating TypeConstrainedDict with valid items."""
     d = TypeConstrainedDict(str, int)
-    d.update({'a': 1, 'b': 2})
-    assert d == {'a': 1, 'b': 2}
+    d.update({"a": 1, "b": 2})
+    assert d == {"a": 1, "b": 2}
+
 
 def test_type_constrained_dict_update_invalid():
     """Test updating TypeConstrainedDict with invalid items."""
     d = TypeConstrainedDict(str, int)
     with pytest.raises(TypeError, match="Invalid value type: got str instead of int"):
-        d.update({'a': 1, 'b': '2'})
+        d.update({"a": 1, "b": "2"})
+
 
 def test_type_constrained_dict_update_kwargs():
     """Test updating TypeConstrainedDict using keyword arguments."""
     d = TypeConstrainedDict(str, int)
     d.update(a=1, b=2)
-    assert d == {'a': 1, 'b': 2}
+    assert d == {"a": 1, "b": 2}
+
 
 def test_type_constrained_dict_update_with_iterable():
     """Test updating TypeConstrainedDict with an iterable of key-value pairs."""
     d = TypeConstrainedDict(str, int)
-    d.update([('a', 1), ('b', 2)])
-    assert d == {'a': 1, 'b': 2}
+    d.update([("a", 1), ("b", 2)])
+    assert d == {"a": 1, "b": 2}
+
 
 def test_type_constrained_dict_update_with_custom_object():
     """Test updating TypeConstrainedDict with a custom object that has a keys method."""
+
     class CustomDict:
+        """Custom dictionary-like object."""
+
         def keys(self):
-            return ['a', 'b']
+            return ["a", "b"]
+
         def __getitem__(self, key):
-            return 1 if key == 'a' else 2
+            return 1 if key == "a" else 2
 
     d = TypeConstrainedDict(str, int)
     d.update(CustomDict())
-    assert d == {'a': 1, 'b': 2}
+    assert d == {"a": 1, "b": 2}
