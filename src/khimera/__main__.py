de53d9b091d092d689b1f72b4b0b244ea488c4ea
Esther Poniatowski <esther.poniatowski@ens.psl.eu>
2025-06-08 12:53:47 +0200

    feat(__main__): Add entry point

---
+++ src/khimera/__main__.py
@@ -0,0 +1,17 @@
+"""
+Command-line entry point for the `khimera` package.
+
+Usage
+-----
+To invoke the package::
+
+    python -m khimera
+
+
+See Also
+--------
+khimera.cli: Command-line interface module for the package.
+"""
+from .cli import app
+
+app()
